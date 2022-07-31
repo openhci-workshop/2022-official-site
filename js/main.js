3e90bba3fc90f8e20de6fbc2024b9afa015b5a7c
Arthur <arthur881225@gmail.com>
2022-07-31 15:51:50 +0800

    Update: Adding everyone's profile picture and link

--- js/main.js
+++ js/main.js
@@ -63,17 +63,23 @@ const groupInfo = {
         member: [{
             "name": "陳昱安",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/coordinator/%E7%B8%BD%E5%8F%AC%E7%B5%84_%E9%99%B3%E6%98%B1%E5%AE%89.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100001495858897"
         }, 
         {
             "name": "洪揚",
             "school": "國立陽明交通大學",
-            "department": "傳播與科技學系"
+            "department": "傳播與科技學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/coordinator/%E7%B8%BD%E5%8F%AC%E7%B5%84_%E6%B4%AA%E6%8F%9A.jpg",
+            "personal_url": "https://hy620.github.io/portfolio/"
         }, 
         {
             "name": "巫芊瑩",
             "school": "國立臺灣大學",
-            "department": "資訊管理學系"
+            "department": "資訊管理學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/coordinator/%E7%B8%BD%E5%8F%AC%E7%B5%84_%E5%B7%AB%E8%8A%8A%E7%91%A9.jpg",
+            "personal_url": "https://www.linkedin.com/in/wpbag"
         }]
     },
     "GENERAL AFFAIRS": {
@@ -81,7 +87,9 @@ const groupInfo = {
         member: [{
             "name": "于庭歡",
             "school": "國立臺灣大學",
-            "department": "經濟系"
+            "department": "經濟系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/general-affair/%E7%B8%BD%E5%8B%99%E7%B5%84_%E4%BA%8E%E5%BA%AD%E6%AD%A1.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100009647740043"
         }]
     },
     "HUMAN RESOURCE": {
@@ -89,17 +97,23 @@ const groupInfo = {
         member: [{
             "name": "許筑涵",
             "school": "國立政治大學",
-            "department": "企業管理學系 / 數位內容學程"
+            "department": "企業管理學系 / 數位內容學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/human-resource/%E4%BA%BA%E4%BA%8B%E7%B5%84_%E8%A8%B1%E7%AD%91%E6%B6%B5.jpg",
+            "personal_url": "http://linkedin.com/in/chu-han-hsu-71bb31186"
         },
         {
             "name": "曾雅婕",
             "school": "國立陽明交通大學",
-            "department": "教育所數位學習組"
+            "department": "教育所數位學習組",
+            "img_url": "https://storage.googleapis.com/openhci-2022/human-resource/%E4%BA%BA%E4%BA%8B%E7%B5%84_%E6%9B%BE%E9%9B%85%E5%A9%95.jpg",
+            "personal_url": ""
         },
         {
             "name": "盧姿惠",
             "school": "國立政治大學",
-            "department": "公共行政學系 / 數位內容學程"
+            "department": "公共行政學系 / 數位內容學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/human-resource/%E4%BA%BA%E4%BA%8B%E7%B5%84_%E7%9B%A7%E5%A7%BF%E6%83%A0.jpg",
+            "personal_url": ""
         }]
     },
     "PHOTOGRAPHY": {
@@ -107,12 +121,16 @@ const groupInfo = {
         member: [{
             "name": "左雅致",
             "school": "崑山科技大學",
-            "department": "資訊管理學系"
+            "department": "資訊管理學系",
+            "img_url": "image/speaker/logo.jpg",
+            "personal_url": ""
         },
         {
             "name": "丁容",
             "school": "國立臺灣大學",
-            "department": "園藝暨景觀學系 景觀遊憩組"
+            "department": "園藝暨景觀學系 景觀遊憩組",
+            "img_url": "image/speaker/logo.jpg",
+            "personal_url": "https://t-jung.wixsite.com/ronronhb?fbclid=IwAR25saUo3c8GCuUm27u0DKDeYSAJsYMhpqCIm_o_tRgS1vqraMJKN28Gy7w"
         }]
     },
     "SPEECH HOST": {
@@ -120,17 +138,23 @@ const groupInfo = {
         member: [{
             "name": "吳冠霖",
             "school": "國立臺灣大學",
-            "department": "工商管理學系"
+            "department": "工商管理學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/speech-host/%E6%BC%94%E8%AC%9B%E7%B5%84_%E5%90%B3%E5%86%A0%E9%9C%96.jpg",
+            "personal_url": "https://www.facebook.com/lingwu0411"
         },
         {
             "name": "劉芷聿",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/speech-host/%E6%BC%94%E8%AC%9B%E7%B5%84_%E5%8A%89%E8%8A%B7%E8%81%BF.jpg",
+            "personal_url": "https://www.linkedin.com/in/chih-yu-liu-500a89173"
         },
         {
             "name": "馮介怡",
             "school": "國立陽明交通大學",
-            "department": "傳播與科技學系"
+            "department": "傳播與科技學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/speech-host/%E6%BC%94%E8%AC%9B%E7%B5%84_%E9%A6%AE%E4%BB%8B%E6%80%A1.jpg",
+            "personal_url": "https://www.facebook.com/thenicolefeng"
         }]
     },
     "SPONSORSHIP": {
@@ -138,12 +162,16 @@ const groupInfo = {
         member: [{
             "name": "高慈憶",
             "school": "國立雲林科技大學",
-            "department": "前瞻學士學位學程"
+            "department": "前瞻學士學位學程",
+            "img_url": "image/speaker/logo.jpg",
+            "personal_url": ""
         },
         {
             "name": "劉佳昀",
             "school": "國立中山大學",
-            "department": "中國文學系"
+            "department": "中國文學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/sponsorship/%E8%B4%8A%E5%8A%A9%E7%B5%84_%E5%8A%89%E4%BD%B3%E6%98%80.jpg",
+            "personal_url": "https://www.facebook.com/laura.liu.16"
         }]
     },
     "WEB SERVICE": {
@@ -151,17 +179,23 @@ const groupInfo = {
         member: [{
             "name": "李欣霏",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/web-service/%E7%B6%B2%E7%AE%A1%E7%B5%84_%E6%9D%8E%E6%AC%A3%E9%9C%8F.jpg",
+            "personal_url": ""
         },
         {
             "name": "楊馥蓉",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/web-service/%E7%B6%B2%E7%AE%A1%E7%B5%84_%E6%A5%8A%E9%A6%A5%E8%93%89.jpg",
+            "personal_url": ""
         },
         {
             "name": "黃柏維",
             "school": "國立臺灣科技大學",
-            "department": "電子工程系"
+            "department": "電子工程系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/web-service/%E7%B6%B2%E7%AE%A1%E7%B5%84_%E9%BB%83%E6%9F%8F%E7%B6%AD.jpg",
+            "personal_url": ""
         }]
     },
     "DESIGN LECTURE": {
@@ -169,17 +203,23 @@ const groupInfo = {
         member: [{
             "name": "翁憶如",
             "school": "國立臺灣師範大學",
-            "department": " 設計系 視覺設計組"
+            "department": " 設計系 視覺設計組",
+            "img_url": "https://storage.googleapis.com/openhci-2022/design-lecture/%E8%A8%AD%E8%A8%88%E6%95%99%E5%AD%B8%E7%B5%84_%E7%BF%81%E6%86%B6%E5%A6%82.jpg",
+            "personal_url": "https://www.linkedin.com/in/louieweng"
         },
         {
             "name": "袁婕",
             "school": "國立清華大學",
-            "department": "服務科學研究所"
+            "department": "服務科學研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/design-lecture/%E8%A8%AD%E8%A8%88%E6%95%99%E5%AD%B8%E7%B5%84_%E8%A2%81%E5%A9%95.jpg",
+            "personal_url": "https://www.facebook.com/chieh.yuan.3/"
         },
         {
             "name": "姚怡均",
             "school": "國立陽明交通大學",
-            "department": "傳播與科技學系"
+            "department": "傳播與科技學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/design-lecture/%E8%A8%AD%E8%A8%88%E6%95%99%E5%AD%B8%E7%B5%84_%E5%A7%9A%E6%80%A1%E5%9D%87.jpg",
+            "personal_url": "https://www.facebook.com/j960095"
         }]
     },
     "TECH LECTURE": {
@@ -187,12 +227,16 @@ const groupInfo = {
         member: [{
             "name": "黃詠瑞",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/tech-lecture/%E6%8A%80%E8%A1%93%E6%95%99%E5%AD%B8%E7%B5%84_%E9%BB%83%E8%A9%A0%E7%91%9E.jpg",
+            "personal_url": ""
         },
         {
             "name": "艾奎華",
             "school": "國立陽明交通大學",
-            "department": "多媒體工程所"
+            "department": "多媒體工程所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/tech-lecture/%E6%8A%80%E8%A1%93%E6%95%99%E5%AD%B8%E7%B5%84_%E8%89%BE%E5%A5%8E%E8%8F%AF.jpg",
+            "personal_url": ""
         }]
     },
     "VISUAL DESIGN": {
@@ -200,32 +244,44 @@ const groupInfo = {
         member: [{
             "name": "鞏持靚",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/visual-design/%E8%A6%96%E8%A6%BA%E7%B5%84_%E9%9E%8F%E6%8C%81%E9%9D%9A.jpg",
+            "personal_url": ""
         },
         {
             "name": "林芷萱",
             "school": "國立政治大學",
-            "department": "數位內容碩士學位學程"
+            "department": "數位內容碩士學位學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/visual-design/%E8%A6%96%E8%A6%BA%E7%B5%84_%E6%9E%97%E8%8A%B7%E8%90%B1.jpg",
+            "personal_url": ""
         },
         {
             "name": "范愷祐",
             "school": "國立政治大學",
-            "department": "廣電系"
+            "department": "廣電系",
+            "img_url": "image/speaker/logo.jpg",
+            "personal_url": "kyofan.notion.site"
         },
         {
             "name": "李悦恩",
             "school": "臺北市立大學",
-            "department": "城市發展系 / 視覺藝術系"
+            "department": "城市發展系 / 視覺藝術系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/visual-design/%E8%A6%96%E8%A6%BA%E7%B5%84_%E6%9D%8E%E6%82%A6%E6%81%A9.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100004182636740"
         },
         {
             "name": "吳泓玉",
             "school": "台南應用科技大學",
-            "department": "服飾設計管理系"
+            "department": "服飾設計管理系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/visual-design/%E8%A6%96%E8%A6%BA%E7%B5%84_%E5%90%B3%E6%B3%93%E7%8E%89.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100002934739757"
         },
         {
             "name": "張瑞庭",
             "school": "國立政治大學",
-            "department": "資訊管理 / 數位內容學程"
+            "department": "資訊管理 / 數位內容學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/visual-design/%E8%A6%96%E8%A6%BA%E7%B5%84_%E5%BC%B5%E7%91%9E%E5%BA%AD.jpg",
+            "personal_url": ""
         }]
     },
     "PUBLICITY": {
@@ -233,22 +289,30 @@ const groupInfo = {
         member: [{
             "name": "林雨翾",
             "school": "國立政治大學",
-            "department": "廣告系 / 數位內容學程"
+            "department": "廣告系 / 數位內容學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/publicity/%E5%AA%92%E9%AB%94%E7%B5%84_%E6%9E%97%E9%9B%A8%E7%BF%BE.jpg",
+            "personal_url": "https://www.facebook.com"
         },
         {
             "name": "林思穎",
             "school": "國立中央大學",
-            "department": "法文系"
+            "department": "法文系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/publicity/%E5%AA%92%E9%AB%94%E7%B5%84_%E6%9E%97%E6%80%9D%E7%A9%8E.jpg",
+            "personal_url": ""
         },
         {
             "name": "許玟心",
             "school": "國立陽明交通大學",
-            "department": "傳播所"
+            "department": "傳播所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/publicity/%E5%AA%92%E9%AB%94%E7%B5%84_%E8%A8%B1%E7%8E%9F%E5%BF%83.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100000490208079"
         },
         {
             "name": "甄曌珞",
             "school": "國立政治大學",
-            "department": "廣告系 / 數位內容學程"
+            "department": "廣告系 / 數位內容學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/publicity/%E5%AA%92%E9%AB%94%E7%B5%84_%E7%94%84%E6%9B%8C%E7%8F%9E.jpg",
+            "personal_url": "https://www.behance.net/chiulokyan"
         }]
     },
     "TEACHING ASSISTANT": {
@@ -256,72 +320,100 @@ const groupInfo = {
         member: [{
             "name": "徐薪淳",
             "school": "國立臺灣大學",
-            "department": "心理所"
+            "department": "心理所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E5%BE%90%E8%96%AA%E6%B7%B3.jpg",
+            "personal_url": "https://www.facebook.com/xinchun.xu/"
         },
         {
             "name": "卓宜璇",
             "school": "國立陽明交通大學",
-            "department": "傳播與科技學系"
+            "department": "傳播與科技學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E5%8D%93%E5%AE%9C%E7%92%87.jpg",
+            "personal_url": "https://www.facebook.com/Choihsuannn"
         },
         {
             "name": "賴宜君",
             "school": "國立陽明交通大學",
-            "department": "傳播研究所"
+            "department": "傳播研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E8%B3%B4%E5%AE%9C%E5%90%9B.jpg",
+            "personal_url": ""
         },
         {
             "name": "許立融",
             "school": "國立陽明交通大學",
-            "department": "傳播研究所"
+            "department": "傳播研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E8%A8%B1%E7%AB%8B%E8%9E%8D.jpg",
+            "personal_url": ""
         },
         {
             "name": "周雨欣",
             "school": "國立清華大學",
-            "department": "藝術與設計學系"
+            "department": "藝術與設計學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E5%91%A8%E9%9B%A8%E6%AC%A3.jpg",
+            "personal_url": "https://www.facebook.com/profile.php?id=100002557852646"
         },
         {
             "name": "金容安",
             "school": "東吳大學",
-            "department": "心理所工商組"
+            "department": "心理所工商組",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E9%87%91%E5%AE%B9%E5%AE%89.jpg",
+            "personal_url": ""
         },
         {
             "name": "王秉中",
             "school": "國立臺灣藝術大學",
-            "department": "圖文傳播藝術學系"
+            "department": "圖文傳播藝術學系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E7%8E%8B%E7%A7%89%E4%B8%AD.jpg",
+            "personal_url": "http://linktr.ee/HaruoWang"
         },
         {
             "name": "黃永雯",
             "school": "國立臺灣大學",
-            "department": "資訊工程學系 / 神經生物與認知科學學程"
+            "department": "資訊工程學系 / 神經生物與認知科學學程",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E6%8A%80%E8%A1%93TA_%E9%BB%83%E6%B0%B8%E9%9B%AF.jpg",
+            "personal_url": ""
         },
         {
             "name": "陳睿芊",
             "school": "國立清華大學",
-            "department": "科管院學士班"
+            "department": "科管院學士班",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E9%99%B3%E7%9D%BF%E8%8A%8A.jpg",
+            "personal_url": ""
         },
         {
             "name": "曾紘遠",
             "school": "國立臺北科技大學",
-            "department": "電子工程系"
+            "department": "電子工程系",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E8%A8%AD%E8%A8%88TA_%E6%9B%BE%E7%B4%98%E9%81%A0.jpg",
+            "personal_url": "https://github.com/johnsonafool"
         },
         {
             "name": "黃晴",
             "school": "國立清華大學",
-            "department": "電機資訊學院學士班"
+            "department": "電機資訊學院學士班",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E6%8A%80%E8%A1%93TA_%E9%BB%83%E6%99%B4.jpg",
+            "personal_url": "https://www.facebook.com/fiona.huang.982"
         },
         {
             "name": "林奕碩",
             "school": "國立台灣大學",
-            "department": "資訊工程研究所"
+            "department": "資訊工程研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E6%8A%80%E8%A1%93TA_%E6%9E%97%E5%A5%95%E7%A2%A9.jpg",
+            "personal_url": ""
         },
         {
             "name": "鄭伯俞",
             "school": "國立陽明交通大學",
-            "department": "資訊工程研究所"
+            "department": "資訊工程研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E6%8A%80%E8%A1%93TA_%E9%84%AD%E4%BC%AF%E4%BF%9E.jpg",
+            "personal_url": ""
         },
         {
             "name": "高語萱",
             "school": "國立臺灣大學",
-            "department": "資訊管理研究所"
+            "department": "資訊管理研究所",
+            "img_url": "https://storage.googleapis.com/openhci-2022/ta/%E6%8A%80%E8%A1%93TA_%E9%AB%98%E8%AA%9E%E8%90%B1.jpg",
+            "personal_url": ""
         }]
     }
 }
@@ -617,8 +709,13 @@ function group_information_insertion() {
                     <div id="collapse_${key}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne" style="width: 100%;">
                         <div class="row group_member">`;
         value.member.forEach((person) => {
+            let profile_link = "";
+            if (person.personal_url !== ""){
+                profile_link = `<a href="${person.personal_url}" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square pl-2"></i></a>`
+            }
             content += `<div class="col-10 col-md-4 group_member_block">
-                            <p><b style="font-size: 16px;">${person.name}</b><br>${person.school}<br>${person.department}</p>
+                            <img class="pb-4 pt-2" style="box-sizing: content-box;" src="${person.img_url}" alt="" width="180" height="180">
+                            <p><b style="font-size: 16px;">${person.name}</b>${profile_link}<br>${person.school}<br>${person.department}</p>
                             </div>`
         })
         content += `</div></div></div>`
